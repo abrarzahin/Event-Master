1822c9cad6c2f1c589da04e2bee01bcc22e335ab
abrar zahin <abrarzahin@yahoo.com>
2019-06-22 11:57:33 -0400

    1 init pro

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// Import React
+import React, { Component } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Event Master</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
