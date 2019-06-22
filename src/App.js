3111a36af3a925e95fc00c542b54d7844c690745
abrar zahin <abrarzahin@yahoo.com>
2019-06-22 12:19:01 -0400

    2 homepage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-// Import React
 import React, { Component } from 'react';
 
 import 'bootstrap/dist/css/bootstrap.css';
 
+import Home from './Home';
+
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>Event Master</h1>
-      </div>
-    );
+    return <Home />;
   }
 }
 
