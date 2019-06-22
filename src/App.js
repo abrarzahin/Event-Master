e46bc7e0800814abef101c8b5d3e80b158e39c9c
abrar zahin <abrarzahin@yahoo.com>
2019-06-22 18:34:17 -0400

    7 router and link added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import Home from './Home';
 import Welcome from './Welcome';
 import Navigation from './Navigation';
+import Register from './Register';
+import { Router } from '@reach/router';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      user: 'Zahin'
+      user: null
     };
   }
 
@@ -16,7 +18,11 @@ class App extends Component {
       <div>
         <Navigation user={this.state.user} />
         {this.state.user && <Welcome user={this.state.user} />}
-        <Home user={this.state.user} />
+
+        <Router>
+          <Home path="/" user={this.state.user} />
+          <Register path="/register" />
+        </Router>
       </div>
     );
   }
