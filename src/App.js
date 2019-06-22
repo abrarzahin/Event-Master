bfea54ef38ae64626c4087ef8af388396d5dd891
abrar zahin <abrarzahin@yahoo.com>
2019-06-22 13:00:04 -0400

    5 welcome page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import Home from './Home';
+import Welcome from './Welcome';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      user: null
+      user: 'Zahin'
     };
   }
 
   render() {
-    return <Home user={this.state.user} />;
+    return (
+      <div>
+        {this.state.user && <Welcome user={this.state.user} />}
+        <Home user={this.state.user} />
+      </div>
+    );
   }
 }
 
