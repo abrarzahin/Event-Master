40800de45896fe5ce1de376e5686a3f88ca29a27
abrar zahin <abrarzahin@yahoo.com>
2019-06-29 23:11:19 -0400

    17 adding meetings

--- src/App.js
+++ src/App.js
@@ -62,6 +62,13 @@ class App extends Component {
       });
   };
 
+   addMeeting = meetingName => {
+    const ref = firebase
+      .database()
+      .ref(`meetings/${this.state.user.uid}`);
+    ref.push({ meetingName: meetingName });
+  };
+
   render() {
     return (
       <div>
@@ -71,7 +78,7 @@ class App extends Component {
         <Router>
           <Home path="/" user={this.state.user} />
           <Login path="/login" />
-          <Meetings path="/meetings" />
+          <Meetings path="/meetings" addMeeting={this.addMeeting} />
           <Register
             path="/register"
             registerUser={this.registerUser}
