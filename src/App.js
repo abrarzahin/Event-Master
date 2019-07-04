1f6b8924d9daa826ca495794fd14385caef7c710
abrar zahin <abrarzahin@yahoo.com>
2019-07-03 23:22:10 -0400

    21 deleting event

--- src/App.js
+++ src/App.js
@@ -105,6 +105,7 @@ class App extends Component {
             path="/meetings"
             meetings={this.state.meetings}
             addMeeting={this.addMeeting}
+            userID={this.state.userID}
           />
           <Register
             path="/register"
