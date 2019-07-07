05708c4a5eead3e4b5b0819fa2e3824025b8c66f
abrar zahin <abrarzahin@yahoo.com>
2019-07-06 23:35:22 -0400

    25 attendee data from database

--- src/Attendees.js
+++ src/Attendees.js
@@ -9,7 +9,31 @@ class Attendees extends Component {
     };
   }
 
-  
+  componentDidMount() {
+    const ref = firebase
+      .database()
+      .ref(
+        `meetings/${this.props.userID}/${
+          this.props.meetingID
+        }/attendees`
+      );
+
+    ref.on('value', snapshot => {
+      let attendees = snapshot.val();
+      let attendeesList = [];
+      for (let item in attendees) {
+        attendeesList.push({
+          attendeeID: item,
+          attendeeName: attendees[item].attendeeName,
+          attendeeEmail: attendees[item].attendeeEmail
+        });
+      }
+      this.setState({
+        displayAttendees: attendeesList
+      });
+    });
+  }
+
   render() {
     return (
       <div className="container mt-4">
